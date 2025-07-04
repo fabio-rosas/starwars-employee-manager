c3afbd55cd4e83dbd4fba635ad47eff1d59bdd16
Fabian Alvarez <ing.alvarezrosas@gmail.com>
2025-07-03 19:58:19 -0600

    [feature] Initial project setup with configuration, Swagger, and environment profiles

---
+++ src/main/java/dev/fabianalvarez/starwarsemployee/StarWarsEmployeeApplication.java
@@ -0,0 +1,13 @@
+package dev.fabianalvarez.starwarsemployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarWarsEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarWarsEmployeeApplication.class, args);
+	}
+
+}
